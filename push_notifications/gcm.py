9530279f799bb3ef2968f69a66ee3ff1a87ac192
Lysi <dominik.lysiak@freenet.de>
2021-05-12 23:01:30 +0200

    Update gcm.py

--- push_notifications/gcm.py
+++ push_notifications/gcm.py
@@ -26,7 +26,10 @@ FCM_OPTIONS_KEYS = [
 ]
 FCM_NOTIFICATIONS_PAYLOAD_KEYS = [
 	"title", "body", "icon", "sound", "badge", "color", "tag", "click_action",
-	"body_loc_key", "body_loc_args", "title_loc_key", "title_loc_args", "android_channel_id"
+	"body_loc_key", "body_loc_args", "title_loc_key", "title_loc_args",
+	"channel_id", "ticker", "sticky", "event_time", "local_only", "notification_priority",
+	"default_sound", "default_vibrate_timings", "default_light_settings", "vibrate_timings",
+	"visibility", "notification_count", "light_settings", "image"
 ]
 
 
